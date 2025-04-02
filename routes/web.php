22adc83927a4a778708431889afe4d5739ec05a6
adeola-olagbenro <olagbenrogabriel@gmail.com>
2025-04-02 21:25:03 +0100

    add admin template

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,19 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\UserActivity;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/user-dashboard', function () {
     $userId = Auth::id();
     $activities = UserActivity::where('user_id',  $userId)
-    ->latest()
-    ->take(10)
-    ->get();
-    return view('dashboard' , compact('activities'));
+        ->latest()
+        ->take(10)
+        ->get();
+    return view('dashboard', compact('activities'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -26,6 +27,12 @@ Route::middleware('auth')->group(function () {
     Route::post('/profile', [ProfileController::class, 'uploadProfilePicture'])->name('profile.upload');
 });
 
+
+Route::middleware(['auth', AdminMiddleware::class])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
 
 
 Route::get('/auth/github/redirect', function () {
